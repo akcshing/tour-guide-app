4a9bec724d8a6f108a15d850f4ba6b6cb8265134
ramosjm <jjramos_2004@hotmail.com>
2019-04-19 11:54:13 +0100

    added back end.

---
+++ back_end/src/main/java/com/codeclan/TourGuideApp/TourGuideAppApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.TourGuideApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourGuideAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourGuideAppApplication.class, args);
+	}
+
+}
