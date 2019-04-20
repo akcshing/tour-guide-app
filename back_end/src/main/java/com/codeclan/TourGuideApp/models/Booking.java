b9984c9425c3f21ab9aa258015767b12396c0aff
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 12:38:51 +0100

    generated getters and setters.

--- back_end/src/main/java/com/codeclan/TourGuideApp/models/Booking.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/models/Booking.java
@@ -20,4 +20,44 @@ public class Booking {
     }
 
     public Booking(){}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public TimeOfDayType getTimeOfDay() {
+        return timeOfDay;
+    }
+
+    public void setTimeOfDay(TimeOfDayType timeOfDay) {
+        this.timeOfDay = timeOfDay;
+    }
+
+    public Attraction getAttraction() {
+        return attraction;
+    }
+
+    public void setAttraction(Attraction attraction) {
+        this.attraction = attraction;
+    }
+
+    public ArrayList<Customer> getTourGroup() {
+        return tourGroup;
+    }
+
+    public void setTourGroup(ArrayList<Customer> tourGroup) {
+        this.tourGroup = tourGroup;
+    }
+
+    public DayType getDay() {
+        return day;
+    }
+
+    public void setDay(DayType day) {
+        this.day = day;
+    }
 }
