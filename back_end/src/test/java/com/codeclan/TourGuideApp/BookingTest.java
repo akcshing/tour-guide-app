76cfcae703b920e2c85f2f6527adb8061211c7fd
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 12:42:07 +0100

    added test file for booking class.

---
+++ back_end/src/test/java/com/codeclan/TourGuideApp/BookingTest.java
@@ -0,0 +1,19 @@
+package com.codeclan.TourGuideApp;
+
+import com.codeclan.TourGuideApp.models.Attraction;
+import com.codeclan.TourGuideApp.models.Booking;
+import com.codeclan.TourGuideApp.models.DayType;
+import com.codeclan.TourGuideApp.models.TimeOfDayType;
+import org.junit.Before;
+
+public class BookingTest {
+
+    private Booking booking;
+    private Attraction attraction;
+
+    @Before
+    public void before(){
+        attraction = new Attraction("Edinburgh Castle","Castlehill","Historic fortress",1.00,10.00,"pichere","historic");
+        booking = new Booking(TimeOfDayType.AFTERNOON,,attraction, DayType.MONDAY);
+    }
+}
