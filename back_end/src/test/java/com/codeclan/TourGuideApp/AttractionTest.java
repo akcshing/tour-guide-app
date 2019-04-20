bf3247aeb82169f21dd1ed15ccd3f245cccba899
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 09:10:45 +0100

    added test file for attraction class.

---
+++ back_end/src/test/java/com/codeclan/TourGuideApp/AttractionTest.java
@@ -0,0 +1,21 @@
+package com.codeclan.TourGuideApp;
+
+import com.codeclan.TourGuideApp.models.Attraction;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AttractionTest {
+
+    private Attraction attraction;
+
+    @Before
+    public void before(){
+        attraction = new Attraction()
+
+    }
+
+    @Test
+    public void canGetName(){
+
+    }
+}
