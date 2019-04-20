05de5bbe2b747200d7bef30b2c84c489083428ad
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 10:24:04 +0100

    can get name test passed.

--- back_end/src/test/java/com/codeclan/TourGuideApp/CustomerTest.java
+++ back_end/src/test/java/com/codeclan/TourGuideApp/CustomerTest.java
@@ -1,11 +1,24 @@
 package com.codeclan.TourGuideApp;
 
+import com.codeclan.TourGuideApp.models.Customer;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CustomerTest {
 
+    private Customer customer;
+
     @Before
     public void before(){
-
+        customer = new Customer("dave","1 house",25,"1234","email.com");
     }
+
+    @Test
+    public void canGetName(){
+        assertEquals("dave",customer.getName());
+    }
+
+
 }
