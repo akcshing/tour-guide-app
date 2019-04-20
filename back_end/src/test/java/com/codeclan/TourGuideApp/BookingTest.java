fe0bd2e1a50f3a148787a9d1b3d1dd9573f5b5e3
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 12:45:21 +0100

    can get time of day test passed.

--- back_end/src/test/java/com/codeclan/TourGuideApp/BookingTest.java
+++ back_end/src/test/java/com/codeclan/TourGuideApp/BookingTest.java
@@ -5,6 +5,9 @@ import com.codeclan.TourGuideApp.models.Booking;
 import com.codeclan.TourGuideApp.models.DayType;
 import com.codeclan.TourGuideApp.models.TimeOfDayType;
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class BookingTest {
 
@@ -14,6 +17,11 @@ public class BookingTest {
     @Before
     public void before(){
         attraction = new Attraction("Edinburgh Castle","Castlehill","Historic fortress",1.00,10.00,"pichere","historic");
-        booking = new Booking(TimeOfDayType.AFTERNOON,,attraction, DayType.MONDAY);
+        booking = new Booking(TimeOfDayType.AFTERNOON,attraction, DayType.MONDAY);
+    }
+
+    @Test
+    public void canGetTimeOfDay(){
+        assertEquals(TimeOfDayType.AFTERNOON, booking.getTimeOfDay());
     }
 }
