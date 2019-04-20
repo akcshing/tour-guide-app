7b5023572e06bc913b0bd5048776fab0fed57be4
ramosjm <jjramos_2004@hotmail.com>
2019-04-20 10:19:34 +0100

    generated getters and setters.

--- back_end/src/main/java/com/codeclan/TourGuideApp/models/Customer.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/models/Customer.java
@@ -20,5 +20,51 @@ public class Customer {
 
     public Customer(){}
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getContacNumber() {
+        return contacNumber;
+    }
+
+    public void setContacNumber(String contacNumber) {
+        this.contacNumber = contacNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
