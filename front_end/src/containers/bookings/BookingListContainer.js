cfaa64f9019e04368c468c57723a40bf509aa9a3
dvioletta <Anita@Pixie.local>
2019-04-22 16:39:11 +0100

    added Booking List container details

---
+++ front_end/src/containers/bookings/BookingListContainer.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import {Link} from 'react-router-dom';
+import CustomerList from "../../components/bookings/BookingList"
+import Request from "../../helpers/request"
+
+class BookingListContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      customers: null
+    }
+  }
+
+  componentDidMount(){
+    let request = new Request()
+    request.get('/bookings').then((data) => {
+      this.setState({bookings: data._embedded.bookings})
+      console.log("hi", data);
+    })
+  }
+
+  render(){
+    console.log("rendering...");
+    return(
+      <div className="booking-list">
+      <h1>Customer List</h1>
+      <Link to="/customers/new">Create Booking</Link>
+      <BookingList bookings = {this.state.bookings}/>
+      </div>
+    )
+  }
+}
+
+export default BookingListContainer;
