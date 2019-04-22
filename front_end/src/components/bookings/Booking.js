c47480e137ad3663485ce6f905fadf7aaaa74612
dvioletta <Anita@Pixie.local>
2019-04-22 15:27:45 +0100

    Added first draft for booking page

--- front_end/src/components/bookings/Booking.js
+++ front_end/src/components/bookings/Booking.js
@@ -3,6 +3,24 @@ import {Link} from 'react-router-dom';
 
 const Booking = ({booking}) => {
 
+  if(!booking) return null;
+  console.log(booking);
+  const urlPieces = booking._link.self.href.split('/');
+  console.log("urlpieces, urlPieces");
+  const id = urlPieces[urlPieces.length-1]
+  return(
+    <React.Fragment>
+      <Link to = {"/bookings/" + id}
+      className="id">
+      {booking.id}
+      </Link>
+      <p>Day: {booking.day}
+      <p>Time Of Day: {booking.timeOfDay}</p>
+      <p>Attraction: {booking.attraction}</p>
+      <p>Customer: {booking.customer}</p>
+      </React.Fragment>
+  )
+
 }
 
 export default Booking;
