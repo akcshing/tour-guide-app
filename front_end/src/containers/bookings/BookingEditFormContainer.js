3a02e8dee891076370910a1d8269f28fba83fa37
dvioletta <Anita@Pixie.local>
2019-04-22 21:35:03 +0100

    updated BookingEdidFormContainer

--- front_end/src/containers/bookings/BookingEditFormContainer.js
+++ front_end/src/containers/bookings/BookingEditFormContainer.js
@@ -1,4 +1,7 @@
 import React, {Component} from "react";
+import BookingEditForm from "../../component/booking/BookingEditForm";
+import Request from "../../helpers/request"
+
 
 
 class BookingEditFormContainer extends Component {
@@ -7,11 +10,36 @@ class BookingEditFormContainer extends Component {
     this.state = {
       customer: null
     }
+    this.handleBookingUpdate = this.handleBookingUpdate.bind(this);
+  }
+
+  componentDidMount(){
+    const request = new Request();
+    request.get("/bookings" + this.props.id).then((booking) => {
+      console.log("data:" , customer);
+      this.setState({customer: customer})
+    });
+    console.log(this.state.booking);
+  }
+
+  handleBookingUpdate(booking){
+    const request = new Request();
+    request.patch("/bookings/" + this.props.id, booking).then(() => {
+      window.location = "/booking/" + this.props.id
+    })
   }
 
   render(){
+    console.log("rendering...");
+    if(!this.state.booking) return null;
+
     return(
-      <h1>Booking EditForm</h1>
+      <div className = "booking-edit-form">
+      <h1>Edit Booking Info</h1>
+      <BookingEditForm
+      booking={this.state.booking}
+      handleBookingUpdate = {this.handleBookingUpdate}/>
+      </div>
     )
   }
 
