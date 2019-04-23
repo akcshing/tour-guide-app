cf126dff7f56d283dc00e2080d188c7b3ad0f1c2
dvioletta <Anita@Pixie.local>
2019-04-23 11:36:03 +0100

    more funtions to create new booking

--- front_end/src/components/bookings/BookingForm.js
+++ front_end/src/components/bookings/BookingForm.js
@@ -2,6 +2,18 @@ import React from 'react';
 
 const BookingForm = (props) => {
 
+  if (!props.customers) return null;
+  if (!props.attractions) return null;
+
+  console.log("i am form props", props);
+
+  const allCustomers = props.customers.map((customer, index) => {
+    return <option key={index} value={index}>{customer.name}</option>
+  })
+  const allAttractions = props.attractions.map((attraction, index) => {
+    return <option key={index} value={index}>{attraction.name}</option>
+  })
+
   function handleSubmit(event){
     event.preventDefault();
     const booking = {
@@ -13,13 +25,27 @@ const BookingForm = (props) => {
     this.props.handleBookingPost(booking)
   }
 
+  function handleCustomerSelect(event){
+    props.handleCustomerSelect(event.target.value);
+  }
+
+  function handleAttractionSelect(event){
+    props.handleAttractionSelect(event.target.value);
+  }
+
   return(
-  <div>
-  <form onSubmit={handleSubmit}>
-    <h1>I AM FORM</h1>
-  </form>
-  </div>
-)
+    <div>
+    <form onSubmit={handleSubmit}>
+    <select onChange= {handleCustomerSelect} name="customer-options">
+    {allCustomers}
+    </select>
+    <select onChange= {handleAttractionSelect} name="attraction-options">
+    {allAttractions}
+    </select>
+    <button type="submit">submit</button>
+    </form>
+    </div>
+  )
 }
 
 export default BookingForm;
--- front_end/src/containers/bookings/BookingFormContainer.js
+++ front_end/src/containers/bookings/BookingFormContainer.js
@@ -5,12 +5,28 @@ import Request from "../../helpers/request"
 class BookingFormContainer extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      attractions: null,
+      customers: null,
+      //selectedCustomer
+      //selectedAttraction
+      //booking
+    }
 
     this.handleBookingPost= this.handleBookingPost.bind(this)
   }
 
   componentDidMount() {
+    const request = new Request();
 
+    request.get('/attractions').then((data) => {
+      this.setState({attractions: data._embedded.attractions})
+    }). then(() => {
+
+    request.get('/customers').then((data)=>{
+      this.setState({customers: data._embedded.customers})
+    });
+  });
   }
 
   handleBookingPost(booking){
@@ -25,6 +41,8 @@ class BookingFormContainer extends Component {
     return(
       <BookingForm
       handleBookingPost={this.handleBookingPost}
+      customers = {this.state.customers}
+      attractions = {this.state.attractions}
       />
     )
   }
