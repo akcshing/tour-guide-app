3fff3301641b656453ac5aa94315346461c81c5f
dvioletta <Anita@Pixie.local>
2019-04-24 13:12:50 +0100

    added resp for attractions

--- back_end/src/main/java/com/codeclan/TourGuideApp/repositories/attractions/AttractionRepositoryCustom.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/repositories/attractions/AttractionRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.codeclan.TourGuideApp.repositories.attractions;
 
+import com.codeclan.TourGuideApp.models.Attraction;
+
+import java.util.List;
+
 public interface AttractionRepositoryCustom {
+
+    List<Attraction> getAttractionByType(String type);
+
 }
--- back_end/src/main/java/com/codeclan/TourGuideApp/repositories/attractions/AttractionRepositoryImpl.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/repositories/attractions/AttractionRepositoryImpl.java
@@ -1,4 +1,37 @@
 package com.codeclan.TourGuideApp.repositories.attractions;
 
+import com.codeclan.TourGuideApp.models.Attraction;
+import com.codeclan.TourGuideApp.models.Booking;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
 public class AttractionRepositoryImpl {
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Attraction> getAttractionByType(String type){
+        List<Attraction> attractions = null;
+        Session session = entityManager.unwrap(Session.class);
+        try{
+            Criteria cr = session.createCriteria(Attraction.class);
+            cr.add(Restrictions.eq("type", type));
+            attractions = cr.list();
+
+        }
+        catch (HibernateException e){
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return attractions;
+    }
 }
