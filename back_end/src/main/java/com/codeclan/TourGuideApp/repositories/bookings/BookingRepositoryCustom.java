a1f3d0b5eb4f3fc4f4bec70d629197d089d3445d
ramosjm <jjramos_2004@hotmail.com>
2019-04-24 10:09:34 +0100

    created get bookings by time of day function.

--- back_end/src/main/java/com/codeclan/TourGuideApp/repositories/bookings/BookingRepositoryCustom.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/repositories/bookings/BookingRepositoryCustom.java
@@ -1,4 +1,9 @@
 package com.codeclan.TourGuideApp.repositories.bookings;
+import com.codeclan.TourGuideApp.enums.TimeOfDayType;
+import com.codeclan.TourGuideApp.models.Booking;
+import java.util.List;
 
 public interface BookingRepositoryCustom {
+
+    List<Booking> getBookingsByTimeOfDay(TimeOfDayType timeOfDay);
 }
--- back_end/src/main/java/com/codeclan/TourGuideApp/repositories/bookings/BookingRepositoryImpl.java
+++ back_end/src/main/java/com/codeclan/TourGuideApp/repositories/bookings/BookingRepositoryImpl.java
@@ -1,4 +1,36 @@
 package com.codeclan.TourGuideApp.repositories.bookings;
 
-public class BookingRepositoryImpl {
+import com.codeclan.TourGuideApp.enums.TimeOfDayType;
+import com.codeclan.TourGuideApp.models.Booking;
+import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+import java.util.List;
+
+public class BookingRepositoryImpl implements BookingRepositoryCustom{
+
+    @Autowired
+    EntityManager entityManager;
+
+    @Transactional
+    public List<Booking> getBookingsByTimeOfDay(TimeOfDayType timeOfDay){
+        List<Booking> bookings = null;
+        Session session = entityManager.unwrap(Session.class);
+        try{
+            Criteria cr = session.createCriteria(Booking.class);
+            cr.add(Restrictions.eq("TimeOfDayType",timeOfDay));
+            bookings = cr.list();
+        }
+        catch (HibernateException e){
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return bookings;
+    }
 }
